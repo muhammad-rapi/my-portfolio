b858aeb445c673faa5ff3d3d3b2ad6bc57c12772
muhammad-rapi <124028853+muhammad-rapi@users.noreply.github.com>
2023-04-15 10:33:26 +0700

    script terbaru

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
+
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+function toggleClassbody() {
+  var body = document.body;
+  "flow" !== body.className ? (body.className = "flow") : (body.className = "");
+}
