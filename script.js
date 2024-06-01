179c9139560382245c9b6080d3bfffebfeefd54c
rafi <admin@MacBook-Pro-admin.local>
2024-06-01 23:03:19 +0700

    new update

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-
-
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
+    nav.classList.toggle("slide");
 });
 
 function toggleClassbody() {
-  var body = document.body;
-  "flow" !== body.className ? (body.className = "flow") : (body.className = "");
+    var body = document.body;
+    "flow" !== body.className
+        ? (body.className = "flow")
+        : (body.className = "");
 }
+
+document.getElementById("download-cv").addEventListener("click", function () {
+    var link = document.createElement("a");
+    link.href = "cv-muhammad-rafi.pdf";
+    link.download = "cv-muhammad-rafi.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
